8da9463a21a868d0b8d8efcfd01ec9370ac9d37b
Kevin Dubois <kevin.dubois@redhat.com>
2019-12-10 11:39:59 +0100

    Formatting

--- buildNativeLinux.sh
+++ buildNativeLinux.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 if [ -z "$GRAALVM_HOME" ]; then
-    export GRAALVM_HOME=/usr/lib/jvm/graalvm/
-fi 
+  export GRAALVM_HOME=/usr/lib/jvm/graalvm/
+fi
 
 # Linux Native
 
 # set default runtime to docker
 runtime='docker'
 
-usage () {
-    echo "usage: ./buildNativeLinux.sh [-r docker|podman]"
+usage() {
+  echo "usage: ./buildNativeLinux.sh [-r docker|podman]"
 }
 
 while [ "$1" != "" ]; do
-    case $1 in
-        -r | --runtime )        shift
-                                runtime=$1
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
-    esac
+  case $1 in
+  -r | --runtime)
     shift
+    runtime=$1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
 done
 
 mvn package -Pnative -Dnative-image.docker-build=true -Dquarkus.native.container-runtime="${runtime}" -DskipTests
--- knpoller.sh
+++ knpoller.sh
@@ -2,7 +2,7 @@
 
 KSVC_URL="$(kubectl -n serverless get ksvc helloquarkusknative -o jsonpath='{.status.url}')"
 
-while true
-do curl $KSVC_URL
-sleep .3
+while true; do
+  curl $KSVC_URL
+  sleep .3
 done
